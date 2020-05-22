76be62246cf063e601dfb1d0209d538fe788559b
Michael Peters <michaelpeterswa@icloud.com>
2020-05-21 22:38:48 -0700

    database, cache, and redirection are now functional

--- src/server.js
+++ src/server.js
@@ -1,60 +1,106 @@
-// letterkennyQuotes api
+// ShortLink
 // Michael Peters
-// July 12, 2019
 
 const express = require('express')
-var cors = require('cors')
-var path = require('path')
-var bp = require('body-parser');
-const ShortUniqueId = require('short-unique-id')
-const sqlite3 = require('sqlite3').verbose()
-
+const cors = require('cors')
+const path = require('path')
+const bp = require('body-parser');
+const shortid = require('shortid')
+const db = require('better-sqlite3')('./db/links.sqlite3')
+const NodeCache = require('node-cache')
 const app = express()
 const socket_server = require('http').Server(app);
 const io = require('socket.io')(socket_server);
 
-const uid = new ShortUniqueId.default()
 // port the app is currently serving to
 const app_port = 6981
 const socket_port = 6982
 
+const linkCache = new NodeCache();
+
 // for socket.io
 socket_server.listen(socket_port)
-// for sqlite3
-let db = new sqlite3.Database('./db/links.sqlite', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Connected to the links database.');
-  });
 
-/////////////////////////////////
+// for better-sqlite3
+db.prepare("CREATE TABLE IF NOT EXISTS links (url TEXT, id TEXT)").run()
 
+// setup data structure
 const link_data = {
     url: "",
     id: ""
 } 
 
-/////////////////////////////////
+//////////////////////////////////
 
 function createID (full_url) {
     link_data.url = full_url
-    link_data.id = uid.randomUUID(6)
-    console.log(link_data)
+    link_data.id = shortid.generate()
+    console.log(`ID (${link_data.id}) created for: ${link_data.url}`)
 }
 
 function sendToDatabase (data) {
-    console.log("Sending to Database")
+    console.log(`Sending to Database: ${data.id}`)
+    var stmt = db.prepare(`INSERT INTO links ("url", "id") VALUES ('${data.url}', '${data.id}')`)
+    stmt.run()
+}
+
+function sendToCache (data) {
+    console.log(`Sending to Cache: ${data.id}`)
+    // cache key is data.id, object is data, ttl is 10k
+    //linkCache.set(data.id, data, 10000)
+}
+
+function getFromCache (id) {
+    console.log("Found Result!")
+    var result = linkCache.get(id)
+    return result
+}
+
+function checkCache (id) {
+    console.log(`Checking cache for ID: ${id}`)
+    return linkCache.has(id)
+}
+
+function getFromDatabase (id) {
+    var result = undefined
+    console.log(`Checking database for ID: ${id}`)
+    var stmt = db.prepare(`SELECT * FROM 'links' WHERE id='${id}'`)
+    result = stmt.get()
+    if(result != undefined) {
+        console.log("Found Result!")
+        return result
+    }
+    else {
+        console.log("Result Not Found")
+        return false
+    }
+}
+
+function checkDatabase (id) {
+    console.log(`Checking database for ID: ${id}`) 
+    return true
+}
+
+// function either returns an object or a false value
+function recieveRequest (id) {
+    if (checkCache(id)) { // if key exists in the cache
+        var cacheResult = getFromCache(id)
+        return cacheResult
+    }
+    else {
+        var dbResult = getFromDatabase(id)
+        return dbResult // could return false
+    }
 }
 
 function displayResult (data) {
-    console.log("Displaying Result")
+    io.emit('new_link', data);
+    console.log("Sent socket.io message")
 }
 
-/////////////////////////////////
+//////////////////////////////////
 
 app.use(cors());
-
 app.use(bp.urlencoded({
     extended: true
   }));
@@ -76,17 +122,28 @@ app.get('/css/sticky-footer.css', function(req, res) {
 app.post('/api', (req, res) => {
     const full_url = req.body.url
     createID(full_url)
+    sendToCache(link_data)
     sendToDatabase(link_data)
-    displayResult(link_data)
     res.redirect('/')
+    displayResult(link_data)
     res.end()
   })
 
-app.use(function (req, res) {
-    res.status(404).sendFile(path.join(__dirname + '/../html/404.html'));
-    //res.status(200).sendFile(path.join(__dirname + '/../css/bootstrap.css'));
-    //res.status(200).sendFile(path.join(__dirname + '/../css/sticky-footer.css'));
-  })
+app.get('/:passed_shortid', function(req, res) {
+    console.log(`Endpoint accessed: /${req.params.passed_shortid}`)
+
+    var id = req.params.passed_shortid
+
+    var result = recieveRequest(id)
+    if(result != false) {
+        console.log(`Redirecting to: ${result.url}`)
+        res.redirect(result.url)
+    }
+    else {
+        console.log("Returning a 404")
+        res.status(404).sendFile(path.join(__dirname + '/../html/404.html'));
+    }
+  });
 
 const app_server = app.listen(app_port, () => console.log(`ShortLink server app listening on port ${app_port}!\n`)) 
 
